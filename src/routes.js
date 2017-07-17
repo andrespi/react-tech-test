d03d09b12c2b67a2a871a8bcc07a02f683cb8236
Andres Espinoza <contact@andresespinoza.com>
2017-07-17 11:25:42 +0200

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './App';
-import MessagesPage from './components/MessagesPage';
-//import AddMessagePage from './components/message/AddMessagePage';
+import MessagesPage from './components/message/MessagesPage';
+import AddMessagePage from './components/message/AddMessagePage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={MessagesPage}/>
+        <Route path="message" component={AddMessagePage}/>
     </Route>
 )
