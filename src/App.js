9c5955c0a29a31e2ae43c8e839d2406e26c25647
Andres Espinoza <contact@andresespinoza.com>
2017-07-17 09:38:52 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="container-fluid">
+            {this.props.children}
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MessagesPage from './components/MessagesPage';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import { Provider } from 'react-redux';
 import Store from './store';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StoreInstance = Store();
 
 ReactDOM.render(
     <Provider store={StoreInstance}>
-        <MessagesPage/>
+        <Router history={browserHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
