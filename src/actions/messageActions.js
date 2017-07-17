2d48a7c89ed8e0e660f2f69ed85db02fdf46d16c
Andres Espinoza <contact@andresespinoza.com>
2017-07-17 13:39:37 +0200

    enhance api

--- src/actions/messageActions.js
+++ src/actions/messageActions.js
@@ -7,10 +7,23 @@ export function createMessage(message) {
     return {type: CREATE_MESSAGE, message}
 }
 
+export function allMessages(messages) {
+    return {type: ALL_MESSAGES, messages};
+}
+
+
 export function addMessage(message) {
     return function (dispatch) {
         return MessageApi.addMessage(message).then(message => {
             dispatch(createMessage(message));
+        })
+    }
+}
+export function getMessages() {
+    return function (dispatch) {
+        return MessageApi.getAllMessages().then(messages => {
+
+            dispatch(allMessages(messages));
         })
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import { Provider } from 'react-redux';
 import Store from './store';
+
+import {getMessages} from './actions/messageActions';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const StoreInstance = Store();
+
+
+StoreInstance.dispatch(getMessages());
 
 ReactDOM.render(
     <Provider store={StoreInstance}>
